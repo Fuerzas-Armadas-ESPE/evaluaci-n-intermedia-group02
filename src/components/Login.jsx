c60250e8248e3916fc5e2fd16a0dff5a7a61912d
Cristian Fernando Idrobo Montalvo <105193365+cfidrobo@users.noreply.github.com>
2024-03-05 00:48:07 -0500

    FuncionalidadesV1
    Se adapto la funcionalidad de un Login para autenticar usuario Docente con clave 123
    
    Se adapto las gestiones de temas para que el docente pueda ingresar nuevos temas, horas, objetivo de materia.

---
+++ src/components/Login.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // Importa useNavigate de react-router-dom
+import '../index.css'; // Importa tus estilos CSS
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate(); // Utiliza el hook useNavigate para la navegación
+
+    const handleLogin = (e) => {
+        e.preventDefault(); // Prevenir el comportamiento por defecto del formulario
+        // Comprueba si las credenciales son correctas
+        if (email === "Docente" && password === "123") {
+            navigate('/show'); // Si las credenciales son correctas, redirige al componente Show
+        } else {
+            alert("Credenciales incorrectas. Por favor, intente de nuevo."); // Muestra un mensaje de error si las credenciales son incorrectas
+        }
+    };
+
+    return (
+        <div className="login-container">
+            <form onSubmit={handleLogin} className="login-form">
+                <h2>Ingreso Docentes</h2>
+                <div className="form-group">
+                    <label htmlFor="email">Usuario</label>
+                    <input type="text" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Contraseña</label>
+                    <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                </div>
+                <button type="submit" className="btn-login">Ingresar</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
